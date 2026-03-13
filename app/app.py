0013dcc9a55a5bc6833a6d94984d83e0a6768121
Ethan Chong <echo0087@student.monash.edu>
2026-03-13 19:32:30 +1100

    fix(app): Fixed variable naming

--- app/app.py
+++ app/app.py
@@ -3,24 +3,24 @@ import os
 
 class AppManager:
     """The main controller for all business logic and data handling."""
-    data_file = ""
-    save_path = ""
 
-    def __init__(self, save_path):
-        self.save_path = save_path
+    def __init__(self, data_file="\\data.json"):
+        self.save_path = AppManager.init_file_path(data_file)
         self._load_data()
 
-    @classmethod
-    def init_file_path(cls,data_file="\\data.json"):
+    @staticmethod
+    def init_file_path(data_file):
         absolute_path = os.path.dirname(__file__)
         relative_path = r"..\data"
-        cls.data_path = os.path.normpath(os.path.join(absolute_path, relative_path))
-        cls.save_file = cls.data_path + data_path
+        data_path = os.path.normpath(os.path.join(absolute_path, relative_path))
+        save_path = data_path + data_file
+
+        return save_path
     
     def _load_data(self):
         """Loads data from the JSON file and populates the object lists."""
         try:
-            with open(self.save_file, 'r') as f:
+            with open(self.save_path, 'r') as f:
                 data = json.load(f)
                 # populate object lists here
         
--- gui/main_dashboard.py
+++ gui/main_dashboard.py
@@ -7,9 +7,8 @@ def launch():
     # Instantiate the "brain" of our app ONCE and store it in the session state.
     # This is crucial so the manager object persists as we switch pages.
     if 'manager' not in st.session_state:
-        AppManager.init_file_path()
-        st.session_state.manager = AppManager(AppManager.save_path)
+        st.session_state.manager = AppManager()
 
     pages_list = []
-    all_page = st.navigation(pages_list, position = "top")
-    all_page.run()
+    # all_page = st.navigation(pages_list, position = "top")
+    # all_page.run()
