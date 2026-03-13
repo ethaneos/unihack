efd22e800dc348827283307975418b6e34b80813
Ethan Chong <echo0087@student.monash.edu>
2026-03-13 19:24:57 +1100

    fix(data): Fixed save path variable naming

--- app/app.py
+++ app/app.py
@@ -3,15 +3,15 @@ import os
 
 class AppManager:
     """The main controller for all business logic and data handling."""
-    data_path = ""
-    save_file = ""
+    data_file = ""
+    save_path = ""
 
-    def __init__(self, save_file):
-        self.save_file = save_file
+    def __init__(self, save_path):
+        self.save_path = save_path
         self._load_data()
 
     @classmethod
-    def init_file_path(cls,data_path="\\data.json"):
+    def init_file_path(cls,data_file="\\data.json"):
         absolute_path = os.path.dirname(__file__)
         relative_path = r"..\data"
         cls.data_path = os.path.normpath(os.path.join(absolute_path, relative_path))
