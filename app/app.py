18e079b74ed17d4908780e663ff7a951256d9b38
Ethan Chong <echo0087@student.monash.edu>
2026-03-13 19:23:05 +1100

    feat(app): Basic functionality implemented

---
+++ app/app.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+class AppManager:
+    """The main controller for all business logic and data handling."""
+    data_path = ""
+    save_file = ""
+
+    def __init__(self, save_file):
+        self.save_file = save_file
+        self._load_data()
+
+    @classmethod
+    def init_file_path(cls,data_path="\\data.json"):
+        absolute_path = os.path.dirname(__file__)
+        relative_path = r"..\data"
+        cls.data_path = os.path.normpath(os.path.join(absolute_path, relative_path))
+        cls.save_file = cls.data_path + data_path
+    
+    def _load_data(self):
+        """Loads data from the JSON file and populates the object lists."""
+        try:
+            with open(self.save_file, 'r') as f:
+                data = json.load(f)
+                # populate object lists here
+        
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            print("Data file not found. Starting with a clean state.")
--- gui/main_dashboard.py
+++ gui/main_dashboard.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from app.app import AppManager
 
 def launch():
     st.set_page_config(layout="wide", page_title="Unihack project")
@@ -6,7 +7,9 @@ def launch():
     # Instantiate the "brain" of our app ONCE and store it in the session state.
     # This is crucial so the manager object persists as we switch pages.
     if 'manager' not in st.session_state:
-        st.session_state.manager = AppManager()
+        AppManager.init_file_path()
+        st.session_state.manager = AppManager(AppManager.save_path)
 
+    pages_list = []
     all_page = st.navigation(pages_list, position = "top")
     all_page.run()
