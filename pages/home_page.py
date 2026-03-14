fa6dc92ef767975d6c624f57bee09508b0dca68e
jer <jeremychi150@gmail.com>
2026-03-14 14:19:20 +1100

    Attemp to fix page links

--- pages/home_page.py
+++ pages/home_page.py
@@ -5,17 +5,14 @@ def show_home_page(manager):
     st.header(":green[Welcome to Subscription Leak Detector!] ✨")
     
     
-    with st.container(border = True):
+    with st.container():
 
-        st.write("""
-        :green[The average household manages 3 subscriptions per month just for streaming.
-        
-        Think of all the services you currently pay for, can you remember them all?
-        This is where SLD comes in to find them for you.]""")
+        st.write(""":green[The average household manages 3 subscriptions per month just for streaming.<br>
+        Think of all the services you currently pay for, can you remember them all? This is where SLD comes in to find them for you.]""")
 
         if st.button("Find my subscriptions", type="primary", width="stretch"):
             st.success("Let's a go!")
-            st.switch_page("pages/upload_page.py")
+            st.switch_page("pages/upload_page")
         
     
     with st.sidebar:
