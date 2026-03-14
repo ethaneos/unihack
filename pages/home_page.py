46a67fb88bb8310f63516baa3cedcebe81d36f5d
jer <jeremychi150@gmail.com>
2026-03-14 14:09:09 +1100

    Refactor home page styling and text color
    Removed custom CSS styling for the home page container and updated text color.

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,24 +1,14 @@
 import streamlit as st
 
 def show_home_page(manager):
-    st.markdown("""
-    <style>
-    .stContainerWithBorder {
-        border: 2px solid lightgreen;
-        border-radius: 5px;
-        padding: 10px;
-        margin-bottom: 10px;
-    }
-    </style>
-    """, unsafe_allow_html=True)
 
     st.header(":green[Welcome to Subscription Leak Detector!] ✨")
     
     
     with st.container(border = True):
-        st.markdown('<div class="stContainerWithBorder">', unsafe_allow_html=True)
+
         st.write("""
-        :lightgreen[The average household manages 3 subscriptions per month just for streaming.]
+        :green[The average household manages 3 subscriptions per month just for streaming.]
         
         :white[Think of all the services you currently pay for, can you remember them all?
         This is where SLD comes in to find them for you.]""")
