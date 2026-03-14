66e5d805bb8abef9129479039536fc47e6fff2dc
jer <jeremychi150@gmail.com>
2026-03-14 17:12:37 +1100

    Text fixes

--- gui/dashboard_page.py
+++ gui/dashboard_page.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 def show_dashboard_page(manager):
 
-    st.title(":white[WTH am I paying for???]")
-    st.header(":white[Welcome to Subscription Leak Detector!] ✨")
+    st.title("WTH am I paying for???")
+    st.header("Welcome to Subscription Leak Detector! ✨")
 
     with st.container():
 
-        st.markdown(":white[The average household manages **3** subscriptions per month just for streaming.]")
-        st.markdown(":white[Think of the services you currently pay for; can you remember them all? This is where SLD comes in to find them for you.]")
+        st.markdown("The average household manages **3** subscriptions per month just for streaming.")
+        st.markdown("Think of the services you currently pay for; can you remember them all? This is where SLD comes in to find them for you.")
         if st.button("Find my subscriptions", type="primary", width="content"):
 
             st.success("Let's a go!")
