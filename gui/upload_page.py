9c70faa214bb5c64d5ed5de962924aa31883b6fb
Ethan Chong <echo0087@student.monash.edu>
2026-03-14 15:37:46 +1100

    feat(gui): changed uploading box size

--- gui/upload_page.py
+++ gui/upload_page.py
@@ -3,9 +3,11 @@ import streamlit as st
 def show_upload_page(manager):
     st.header("Upload")
 
-    uploaded_file = st.file_uploader("Upload a CSV")
-    if uploaded_file is not None:
-        st.caption("File uploaded")
+    col1, col2, col3 = st.columns([1,2,1])
+    with col2:
+        uploaded_file = st.file_uploader("Upload a CSV")
+        if uploaded_file is not None:
+            st.caption("File uploaded")
     
     with st.sidebar:
         st.markdown("#")
