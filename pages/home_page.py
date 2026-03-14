cee945e1efca3509af6e7c7ee866d9cbf535037b
jer <jeremychi150@gmail.com>
2026-03-14 14:12:04 +1100

    Attempt to fix switch page button

--- pages/home_page.py
+++ pages/home_page.py
@@ -8,14 +8,14 @@ def show_home_page(manager):
     with st.container(border = True):
 
         st.write("""
-        :green[The average household manages 3 subscriptions per month just for streaming.]
+        :green[The average household manages 3 subscriptions per month just for streaming.
         
-        :white[Think of all the services you currently pay for, can you remember them all?
+        Think of all the services you currently pay for, can you remember them all?
         This is where SLD comes in to find them for you.]""")
 
         if st.button("Find my subscriptions", type="primary", width="stretch"):
             st.success("Let's a go!")
-            st.switch_page("pages/upload")
+            st.switch_page("pages/upload_page.py")
         
     
     with st.sidebar:
