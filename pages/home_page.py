93382a37ff07ffbfb961e92621160451fad80a87
jer <jeremychi150@gmail.com>
2026-03-14 12:58:20 +1100

    Enhance home page header and content styling
    Updated header styling and modified text for clarity.

--- pages/home_page.py
+++ pages/home_page.py
@@ -2,34 +2,13 @@ import streamlit as st
 
 def show_home_page(manager):
 
-    st.header("Welcome to Subscription Leak Detector! ✨")
+    st.header(":green[Welcome to Subscription Leak Detector!] ✨")
     
-    st.markdown("""
-    <style>
-    /* Main background */
-    .stApp {
-        background-color: #f0fdf4; /* Very light mint green */
-    }
-    
-    /* Style the metric cards */
-    [data-testid="stMetricValue"] {
-        color: #166534; /* Dark forest green */
-    }
-    
-    /* Colorful Sidebar */
-    [data-testid="stSidebar"] {
-        background-color: #dcfce7; /* Soft pastel green */
-    }
-    </style>
-    """, unsafe_allow_html = True)
     
     with st.container(border = True):
-        st.write("""The average household manages 3 subscriptions per month just for streaming.
+        st.write(""":lightgreen[The average household manages 3 subscriptions per month just for streaming.
         Think of all the services you pay for, can you remember them all?
-        This is where SLD comes in to find them for you.""")
-
-        #st.page_link("upload", label="View Your Subscriptions", icon="📊")
-        
+        This is where SLD comes in to find them for you.]""")        
         
     
     with st.sidebar:
