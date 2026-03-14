dad6067cc3a11c1300f24cc5f9b0550109b72c9b
jer <jeremychi150@gmail.com>
2026-03-14 15:22:40 +1100

    Allow only csv format

--- gui/upload_page.py
+++ gui/upload_page.py
@@ -3,7 +3,7 @@ import streamlit as st
 def show_upload_page(manager):
     st.header("Upload")
 
-    uploaded_file = st.file_uploader("Upload a CSV")
+    uploaded_file = st.file_uploader("Upload a CSV", type="csv", accept_multiple_files=True)
     if uploaded_file is not None:
         st.caption("File uploaded")
     
