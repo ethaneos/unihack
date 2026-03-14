030ffb5e7844e867d3cc0185803088a50ad57a28
jer <jeremychi150@gmail.com>
2026-03-14 12:02:02 +1100

    More Gui
    Added some testing gui elements

--- gui/home_page.py
+++ gui/home_page.py
@@ -2,7 +2,20 @@ import streamlit as st
 
 def show_home_page(manager):
     st.header("Home Page")
+    st.write("Welcome to Subscription Leak Detector!")
 
+    col1 = st.columns(1)
+
+    with col1:
+        st.button("test button", type="primary", use_container_width=True)
+
+    input = st.text_input("What is your name?")#text box
+    
+    if st.button("Say Hello"):
+        if input:
+            st.write(f"Hello, {input}!")
+        else:
+            st.warning("Please enter a name first.")
     
     with st.sidebar:
         st.markdown("#")
