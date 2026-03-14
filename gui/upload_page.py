d24ce8f6592d0b605c7065d31baf1f25ae627c5c
jer <jeremychi150@gmail.com>
2026-03-14 15:27:57 +1100

    Print submitted files

--- gui/upload_page.py
+++ gui/upload_page.py
@@ -1,11 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 def show_upload_page(manager):
     st.header("Upload")
 
-    uploaded_file = st.file_uploader("Upload a CSV", type="csv", accept_multiple_files=True)
-    if uploaded_file is not None:
-        st.caption("File uploaded")
+    uploaded_files = st.file_uploader("Upload a CSV", type="csv", accept_multiple_files=True)
+    if uploaded_files is not None:
+        for file in uploaded_files:
+            df = pd.read_csv(file)
+            st.write(df)
     
     with st.sidebar:
         st.markdown("#")
