5f55ea5a59990c74d8acf0f3800a603a8750f2f6
jer <jeremychi150@gmail.com>
2026-03-14 15:18:00 +1100

    Added some upload area

--- gui/upload_page.py
+++ gui/upload_page.py
@@ -3,6 +3,9 @@ import streamlit as st
 def show_upload_page(manager):
     st.header("Upload")
 
+    uploaded_file = st.file_uploader("Upload a CSV")
+    if uploaded_file is not None:
+        st.caption("File uploaded")
     
     with st.sidebar:
         st.markdown("#")
