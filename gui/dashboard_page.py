6ad34a454555c2071c57b3ed233378519578ac39
jer <jeremychi150@gmail.com>
2026-03-14 14:33:27 +1100

    rev

--- gui/dashboard_page.py
+++ gui/dashboard_page.py
@@ -1,8 +1,17 @@
 import streamlit as st
 
 def show_dashboard_page(manager):
-    st.header("Welcome <<user>>")
 
+    st.header(":green[Welcome to Subscription Leak Detector!] ✨")
+    
+    with st.container():
+
+        st.write(""":green[The average household manages 3 subscriptions per month just for streaming.<br>
+        Think of all the services you currently pay for, can you remember them all? This is where SLD comes in to find them for you.]""")
+
+        if st.button("Find my subscriptions", type="primary", width="stretch"):
+            st.success("Let's a go!")
+            st.switch_page("gui/upload_page.py")
     
     with st.sidebar:
         st.markdown("#")
