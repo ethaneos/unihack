e382d5d8fbefea43a83b757441e51f7e5111bac7
jer <jeremychi150@gmail.com>
2026-03-14 17:13:51 +1100

    Fix reading files

--- gui/upload_page.py
+++ gui/upload_page.py
@@ -6,11 +6,10 @@ def show_upload_page(manager):
 
     col1, col2, col3 = st.columns([1,2,1])
     with col2:
-        uploaded_files = st.file_uploader("Upload a CSV", type="csv", accept_multiple_files=False)
-        if uploaded_files is not None:
-            for file in uploaded_files:
-                df = pd.read_csv(file)
-                st.write(df)
+        uploaded_file = st.file_uploader("Upload a CSV", type="csv", accept_multiple_files=False)
+        if uploaded_file is not None:
+            df = pd.read_csv(uploaded_file)
+            st.write(df)
     
     with st.sidebar:
         st.markdown("#")
